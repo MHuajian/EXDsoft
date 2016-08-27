679c35bcf44ff403ac429ad325c9e7feafa78438
Faker <MHuajian@163.com>
2016-08-27 20:00:42 +0800

    publish-EXDsoft
    易信达软件的功能介绍网页

---
+++ ad-gallery/jquery-top.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(){
+  $("i:first").hover(
+  function(){
+	$(".cpjs").css("display","block");
+},
+  function(){
+	$(".cpjs").css("display","none"); 
+	  }
+)
+}); 
